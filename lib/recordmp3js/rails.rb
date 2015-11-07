9b04e77fc291d2ce837624b0895a2fe2826062c2
Denis <endenis@gmail.com>
2015-11-07 17:31:46 +0100

    First changes

---
+++ lib/recordmp3js/rails.rb
@@ -0,0 +1,7 @@
+require "recordmp3js/rails/version"
+
+module Recordmp3js
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/recordmp3js/rails/version.rb
@@ -0,0 +1,5 @@
+module Recordmp3js
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
